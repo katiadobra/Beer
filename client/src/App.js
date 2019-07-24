a76b804916ee56bbf3225e303a97805221bfd5f9
katiadobra <katierybalko@gmail.com>
2019-07-24 19:31:31 +0200

    🐣 set-up react app

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Zeus</h1>
+        <p>Welcome to Zeus project!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
