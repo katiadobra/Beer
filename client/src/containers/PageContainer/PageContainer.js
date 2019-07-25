07bb92f35d53abccfe386c3199798f0c92985e98
katiadobra <katierybalko@gmail.com>
2019-07-25 19:52:21 +0200

    add prop-types

--- client/src/containers/PageContainer/PageContainer.js
+++ client/src/containers/PageContainer/PageContainer.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 import Aux from '../../hoc/Aux';
 import './PageContainer.scss';
@@ -12,5 +13,9 @@ class PageContainer extends Component {
     );
   }
 }
+
+PageContainer.propTypes = {
+  type: PropTypes.string
+};
 
 export default PageContainer;
