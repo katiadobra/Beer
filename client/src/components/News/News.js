c7304bfba8bd8b1707193c9f0d42eec887784f3b
katiadobra <katierybalko@gmail.com>
2019-07-25 20:08:55 +0200

    News

---
+++ client/src/components/News/News.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const News = () => {
+  return (
+    <section>
+      News
+      <button aria-label="Check more">Check more</button>
+    </section>
+  );
+};
+
+export default News;
